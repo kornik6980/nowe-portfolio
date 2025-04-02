3da9b35c7c5dd5d571f899004a71b2683f5c733d
czare <czarek.nartowskI@gmail.com>
2025-04-02 18:27:56 +0200

    remove image and link from home page, simplifying the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Home() {
-	return (
-		<div className="w-full h-screen flex items-center justify-center">
-			<Link href="https://www.tiktok.com/@kornik2212">
-				<Image fill alt="image" src="/static/images/mclovin.jpg" />
-			</Link>
-		</div>
-	);
+	return <div></div>;
 }
