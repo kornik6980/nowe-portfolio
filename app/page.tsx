75e71d354676ac25003aa2db1c3d9153847a3c91
czare <czarek.nartowskI@gmail.com>
2025-04-02 19:08:48 +0200

    Updates the main page to include Hero, Info, and Featured components, implements Lenis

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+import Hero from "@/components/Hero";
+import Info from "@/components/Info";
+import Featured from "@/components/Featured";
+
 export default function Home() {
-	return <div></div>;
+	useEffect(() => {
+		const lenis = new Lenis();
+		function raf(time: number) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+		requestAnimationFrame(raf);
+	});
+	return (
+		<>
+			<Hero />
+			<Info />
+			<Featured />
+		</>
+	);
 }
